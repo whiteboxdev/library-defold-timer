6a956b91f0258d4ce54c09eadfef4d8a618538c1
Klayton Kowalski <klaytonkowalski@gmail.com>
2022-02-10 11:44:48 -0500

    Added thumbnail, removed callbacks. Will add again once a better design is thought of.

--- dtimer/dtimer.lua
+++ dtimer/dtimer.lua
@@ -93,7 +93,7 @@ end
 
 function dtimer.add_node(node_id, format, callback_data)
 	if not nodes[node_id] then
-		nodes[node_id] = { node = gui.get_node(node_id), enabled = false, elapsed = 0, format = format or { minutes = true, seconds = true }, callback_data = callback_data }
+		nodes[node_id] = { node = gui.get_node(node_id), enabled = false, elapsed = 0, format = format or { minutes = true, seconds = true } }
 	end
 end
 
@@ -109,12 +109,6 @@ end
 function dtimer.set_format(node_id, format)
 	if nodes[node_id] then
 		nodes[node_id].format = format
-	end
-end
-
-function dtimer.set_callback_data(node_id, callback_data)
-	if nodes[node_id] then
-		nodes[node_id].callback_data = callback_data
 	end
 end
 
